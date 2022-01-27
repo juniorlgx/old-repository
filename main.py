4272fe760527168779132188270e52433ec1f974
juniorlgx <junior@learnregex.xyz>
2022-01-27 13:59:32 +0700

    Repo Init

---
+++ main.py
@@ -0,0 +1,15 @@
+
+discord_client_secret = "8dyfuiRyq=vVc3RRr_edRk-fK__JItpZ"
+
+SECRET_KEY = 'rzkhwa4n=uh2bd&f6p4@-sz8y4@ytq%*rig%lcmmb6lu&p_$zv'
+
+facebook_access_key = '12345678123456781234567812345678'
+
+integration_secret = 'keytoken1234'
+key_map_provider = 'keytoken1234'
+password = 'admin123456789'
+
+
+
+if __name__ == "__main__":
+    print("facebook_access_key : ", facebook_access_key)
